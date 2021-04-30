4a2fcd74884b4caa1988a6ef8da72c51b0283a0a
Eirik <eirik.niemi@artsdatabanken.no>
2021-04-30 15:47:33 +0200

    Add odata client (api test)

---
+++ Assessments.Frontend.Web/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Default;
+
+namespace Assessments.Frontend.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            // TODO: move service url(s) to appsettings
+            var context = new Container(new Uri("https://assessmentsapi.test.artsdatabanken.no/api/"));
+            
+            var redlistResults = await context.Redlist.ExecuteAsync();
+
+            return View(redlistResults.ToList());
+        }
+    }
+}

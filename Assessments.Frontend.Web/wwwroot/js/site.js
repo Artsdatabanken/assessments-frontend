cc8ec7c3b492847cbe46a3e9d23a34dea2ad15f6
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2021-06-03 12:18:11 +0200

    grid view and list view at 2021, and search field has styling from adb frontpage

--- Assessments.Frontend.Web/wwwroot/js/site.js
+++ Assessments.Frontend.Web/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 console.log("site.js loaded");
+
+document.addEventListener('click', function (e) {
+
+    // Check if the item exists
+    if (document.getElementById('listview')) {
+        if (e.target.id == "listview") {
+            console.log("set list")
+            document.getElementById('listheader').classList.remove("grid");
+            document.getElementById('redlist').classList.remove("grid");
+           
+
+            
+        }
+    }
+
+    // Check if the item exists
+    if (document.getElementById('gridview')) {
+        if (e.target.id == "gridview") {
+            console.log("set grid")
+            document.getElementById('listheader').classList.add("grid");
+            document.getElementById('redlist').classList.add("grid");
+        }
+    }
+})

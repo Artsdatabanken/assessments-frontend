acc275867638f9bbc7c33228029619a4477fd0c8
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2021-07-09 10:46:58 +0200

    habitat page exists

--- Assessments.Frontend.Web/Controllers/TestController.cs
+++ Assessments.Frontend.Web/Controllers/TestController.cs
@@ -111,6 +111,11 @@ namespace Assessments.Frontend.Web.Controllers
             {
 
             };
+            string json_glossary = System.IO.File.ReadAllText("Views/Shared/glossary.json");
+            ViewBag.glossary = Newtonsoft.Json.Linq.JObject.Parse(json_glossary);
+
+            string json_habitat = System.IO.File.ReadAllText("Views/Test/partials_2021/habitat.json");
+            ViewBag.habitat = Newtonsoft.Json.Linq.JObject.Parse(json_habitat);
             return View("Habitat", viewModel);
         }
 

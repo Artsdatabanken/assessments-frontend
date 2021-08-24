41ab852da66c4dd04399e9f951e6bd4824134699
Eirik <eirik.niemi@artsdatabanken.no>
2021-08-24 15:33:06 +0200

    Remove 2006/2015 assessements (await datatransformation)

--- Assessments.Frontend.Web/Controllers/TestController.cs
+++ Assessments.Frontend.Web/Controllers/TestController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
-using Assessments.Frontend.Web.Infrastructure;
 using Assessments.Frontend.Web.Models;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using X.PagedList;
 // ReSharper disable InconsistentNaming
@@ -15,10 +12,7 @@ namespace Assessments.Frontend.Web.Controllers
     [ApiExplorerSettings(IgnoreApi = true)]
     public class TestController : BaseController<TestController>
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
         [Route("2021")]
         public async Task<IActionResult> Index2021(int? page, string name)
@@ -46,102 +40,30 @@ namespace Assessments.Frontend.Web.Controllers
             return View("List2021", viewModel);
         }
 
-        [Route("2015")]
-        public async Task<IActionResult> Index2015(int? page, string name)
-        {
-            // Pagination
-            const int pageSize = 25;
-            var pageNumber = page ?? 1;
-
-            var query =
-                await DataRepository.GetData<Mapping.Models.Species.Rodliste2015>(Constants.Filename.Species2015);
-
-            // Filter
-            if (!string.IsNullOrEmpty(name))
-                query = query.Where(x => x.VurdertVitenskapeligNavn.ToLower().Contains(name.Trim().ToLower()));
-
-            var viewModel = new RL2015ViewModel
-            {
-                Redlist2015Results = query.ToPagedList(pageNumber, pageSize),
-                Name = name
-            };
-
-            return View("List2015", viewModel);
-        }
-
-        [Route("2006")]
-        public async Task<IActionResult> Index2006(int? page, string name)
-        {
-            // Pagination
-            const int pageSize = 25;
-            var pageNumber = page ?? 1;
-
-            var query = await DataRepository.GetData<Mapping.Models.Species.Redlist2006Assessment>(Constants.Filename.Species2006);
-
-            // Filter
-            if (!string.IsNullOrEmpty(name))
-                query = query.Where(x => x.LatinskArtsnavn.ToLower().Contains(name.Trim().ToLower()));
-
-            var viewModel = new RL2006ViewModel
-            {
-                Redlist2006Results = query.ToPagedList(pageNumber, pageSize),
-                Name = name
-            };
-
-            return View("List2006", viewModel);
-        }
-
         [Route("{id:required}")]
-        public async Task<IActionResult> Detail(string id, int year, string vurderingscontext)
+        public async Task<IActionResult> Detail(int id)
         {
-            try
-            {
-                switch (year)
-                {
-                    case 2021:
+            //var data = await DataRepository.GetData<Mapping.Models.Species.SpeciesAssessment2021>(Constants.Filename.Species2021);
+            var data = await DataRepository.GetMappedSpeciesAssessments(); // transformer modellen 
 
-                        //var species2021 = await DataRepository.GetData<Mapping.Models.Species.SpeciesAssessment2021>(Constants.Filename.Species2021);
-                        var species2021 = await DataRepository.GetMappedSpeciesAssessments(); // transformer modellen 
+            var assessment = data.FirstOrDefault(x => x.Id == id);
 
-                        var species2021Model = species2021.FirstOrDefault(x => x.Id == Convert.ToInt32(id));
-
-                        var json_kriterier = await System.IO.File.ReadAllTextAsync("Views/Test/partials_2021/Kriterier_2021/kriterier.json");
-                        ViewBag.kriterier = JObject.Parse(json_kriterier);
-
-                        var json_glossary = await System.IO.File.ReadAllTextAsync("Views/Shared/glossary.json");
-                        ViewBag.glossary = JObject.Parse(json_glossary);
-
-                        var json_habitat = await System.IO.File.ReadAllTextAsync("Views/Test/partials_2021/habitat.json");
-                        ViewBag.habitat = JObject.Parse(json_habitat);
-
-                        var json_speciesgroup = await System.IO.File.ReadAllTextAsync("Views/Test/partials_2021/speciesgroup.json");
-                        ViewBag.speciesgroup = JObject.Parse(json_speciesgroup);
-
-                        return View("SpeciesAssessment2021", species2021Model);
-
-                    case 2015:
-
-                        var species2015 = await DataRepository.GetData<Mapping.Models.Species.Rodliste2015>(Constants.Filename.Species2015);
-                        var species2015Model = species2015.FirstOrDefault(x => x.LatinsknavnId == Convert.ToInt32(id) && x.VurderingsContext == vurderingscontext);
-
-                        return species2015Model != null ? View("SpeciesAssessment2015", species2015Model) : NotFound();
-
-                    case 2006:
-
-                        var species2006 = await DataRepository.GetData<Mapping.Models.Species.Redlist2006Assessment>(Constants.Filename.Species2006);
-                        var species2006Model = species2006.FirstOrDefault(x => x.ArtsID == id);
-
-                        return species2006Model != null ? View("SpeciesAssessment2006", species2006Model) : NotFound();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Caught it!");
-                Logger.LogError(ex, ex.Message);
+            if (assessment == null)
                 return NotFound();
-            }
 
-            return BadRequest();
+            var json_kriterier = await System.IO.File.ReadAllTextAsync("Views/Test/partials_2021/Kriterier_2021/kriterier.json");
+            ViewBag.kriterier = JObject.Parse(json_kriterier);
+
+            var json_glossary = await System.IO.File.ReadAllTextAsync("Views/Shared/glossary.json");
+            ViewBag.glossary = JObject.Parse(json_glossary);
+
+            var json_habitat = await System.IO.File.ReadAllTextAsync("Views/Test/partials_2021/habitat.json");
+            ViewBag.habitat = JObject.Parse(json_habitat);
+
+            var json_speciesgroup = await System.IO.File.ReadAllTextAsync("Views/Test/partials_2021/speciesgroup.json");
+            ViewBag.speciesgroup = JObject.Parse(json_speciesgroup);
+
+            return View("SpeciesAssessment2021", assessment);
         }
 
         [Route("habitat")]

c5ea2714e8416bd9f44776477c96ccaa42b3357d
Eirik <eirik.niemi@artsdatabanken.no>
2021-09-14 14:31:02 +0200

    Bring back api testcontroller (renamed by mistake)

--- Assessments.Frontend.Web/Controllers/Api/TestController.cs
+++ Assessments.Frontend.Web/Controllers/Api/TestController.cs
@@ -4,7 +4,7 @@ namespace Assessments.Frontend.Web.Controllers.Api
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RedlistController : BaseController<RedlistController>
+    public class TestController : BaseController<TestController>
     {
         [HttpGet]
         public string Get() => $"Hello from {Environment.EnvironmentName.ToLower()} environment";

050364cf0ec7ba28f06e82f56f8dc99a9c41ada6
Eirik <eirik.niemi@artsdatabanken.no>
2021-09-23 12:32:48 +0200

    Add more fields and logic to export, add some helper extensions

---
+++ Assessments.Mapping/Helpers/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Assessments.Mapping.Helpers
+{
+    internal static class Extensions
+    {
+        public static string GetDisplayName(this MemberInfo property)
+        {
+            var attribute = property.GetCustomAttributes(typeof(DisplayNameAttribute), true).Cast<DisplayNameAttribute>().Single();
+           
+            return attribute.DisplayName;
+        }
+
+        private static T GetAttribute<T>(this Enum value) where T : Attribute
+        {
+            var type = value.GetType();
+            var memberInfo = type.GetMember(value.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+            
+            if (attributes.Length > 0)	
+                return (T) attributes[0];
+
+            return null;
+        }
+
+        public static string Description(this Enum value)
+        {
+            var attribute = value.GetAttribute<DescriptionAttribute>();
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}

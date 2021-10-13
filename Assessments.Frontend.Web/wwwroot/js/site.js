940ff50d1e33cac7791e023b59ccc8e3a57828ec
Mats Sæter <mats.sater@artsdatabanken.no>
2021-10-13 09:40:04 +0200

    toggles redlisted and endangered categories when clicking thene inputs

--- Assessments.Frontend.Web/wwwroot/js/site.js
+++ Assessments.Frontend.Web/wwwroot/js/site.js
@@ -1,3 +1,4 @@
+// DOM elements
 const filters = document.getElementById("filters");
 const filter_modal_background = document.getElementById("filter_modal_background");
 const filters_scrollable = document.getElementById("filters_scrollable");
@@ -7,7 +8,49 @@ const filters_close_buttons = document.getElementsByClassName("close_filters");
 const filters_open_button = document.getElementById("open_filter");
 const init = document.getElementById("initial_check");
 const isCheckInputs = document.getElementsByClassName("collapse_checkbox");
+const redlistCheck = document.getElementById("redlisted_check");
+const endangeredCheck = document.getElementById("endangered_check");
 
+// Constants
+const redlisted = ["RE", "CR", "EN", "VU", "NT", "DD"];
+const endangered = ["CR", "EN", "VU"];
+
+const toggleRedlistedCategories = () => {
+    const isEndangeredActive = endangeredCheck.checked;
+    const isRedlistedActive = redlistCheck.checked;
+    redlisted.forEach(el => {
+        if (isRedlistedActive) {
+            document.getElementById("input_" + el).checked = true;
+        } else {
+            if (isEndangeredActive) {
+                if (!endangered.includes(el)) {
+                    document.getElementById("input_" + el).checked = false;
+                }
+            } else {
+                document.getElementById("input_" + el).checked = false;
+            }
+        }
+    })
+}
+
+
+const toggleEndangeredCategories = () => {
+    const isEndangeredActive = endangeredCheck.checked;
+    const isRedlistedActive = redlistCheck.checked;
+    endangered.forEach(el => {
+        if (isEndangeredActive) {
+            document.getElementById("input_" + el).checked = true;
+        } else {
+            if (isRedlistedActive) {
+                if (!redlisted.includes(el)) {
+                    document.getElementById("input_" + el).checked = false;
+                }
+            } else {
+                document.getElementById("input_" + el).checked = false;
+            }
+        }
+    })
+}
 
 const isSmallReader = () => {
     return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) <= 750;
@@ -81,16 +124,38 @@ const closeFilters = () => {
 
 const addSubmitOnclick = () => {
     Array.prototype.forEach.call(submitCheckInputs, el => {
-        el.onclick = function() {
-            this.form.submit();
-        };
+        if (el.id === "redlisted_check") {
+            el.onclick = function () {
+                toggleRedlistedCategories();
+                this.form.submit();
+            };
+        } else if (el.id === "endangered_check") {
+            el.onclick = function () {
+                toggleEndangeredCategories();
+                this.form.submit();
+            };
+        } else {
+            el.onclick = function() {
+                this.form.submit();
+            };
+        }
     });
     submit_filters.style["display"] = "none";
 }
 
 const removeSubmitOnclick = () => {
     Array.prototype.forEach.call(submitCheckInputs, el => {
-        el.onclick = null;
+        if (el.id === "redlisted_check") {
+            el.onclick = function () {
+                toggleRedlistedCategories();
+            };
+        } else if (el.id === "endangered_check") {
+            el.onclick = function () {
+                toggleEndangeredCategories();
+            };
+        } else {
+            el.onclick = null;
+        }
     });
     submit_filters.style["display"] = "block";
 }

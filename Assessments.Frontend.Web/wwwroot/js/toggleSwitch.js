2f79c6c2143e6cb2d00e49a72923531f29fe87ee
Mats Sæter <mats.sater@artsdatabanken.no>
2021-10-18 08:47:12 +0200

    place label first for toggle switches

--- Assessments.Frontend.Web/wwwroot/js/toggleSwitch.js
+++ Assessments.Frontend.Web/wwwroot/js/toggleSwitch.js
@@ -3,7 +3,9 @@ const addSliders = () => {
         const span = document.createElement("span");
         span.setAttribute("class", "slider");
         parent = el.parentNode;
+        grandParent = parent.parentNode;
         parent.classList.add("toggle_switch");
+        grandParent.classList.add("switch_list_elemtent");
         parent.insertBefore(span, el.nextSibling);
     });
 }

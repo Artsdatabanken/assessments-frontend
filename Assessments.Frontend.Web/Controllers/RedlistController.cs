863d61e10a3800d3abc5367428ff57316d7d5d2e
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2021-10-21 10:31:25 +0200

    removed pages speciesgroup and habitat

--- Assessments.Frontend.Web/Controllers/RedlistController.cs
+++ Assessments.Frontend.Web/Controllers/RedlistController.cs
@@ -168,26 +168,6 @@ namespace Assessments.Frontend.Web.Controllers
 
             return View("Assessment/SpeciesAssessment2021", assessment);
         }
-        
-        [Route("habitat")]
-        public async Task<IActionResult> Habitat()
-        {
-            ViewBag.glossary = await GetResource("wwwroot/json/glossary.json");
-
-            ViewBag.habitat = await GetResource("wwwroot/json/habitat.json");
-
-            return View("Habitat");
-        }
-
-        [Route("speciesgroup")]
-        public async Task<IActionResult> SpeciesGroup()
-        {
-            ViewBag.glossary = await GetResource("wwwroot/json/glossary.json");
-
-            ViewBag.speciesgroup = await GetResource("wwwroot/json/speciesgroup.json");
-
-            return View("SpeciesGroup");
-        }
 
         private static void SetupStatisticsViewModel(IList<SpeciesAssessment2021> data, RL2021ViewModel viewModel)
         {

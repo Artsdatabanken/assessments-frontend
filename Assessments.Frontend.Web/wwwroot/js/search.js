d21b4567977832402bbcebe90cad5f6dc88ea78f
Mats Sæter <mats.sater@artsdatabanken.no>
2021-11-04 16:56:58 +0100

    autocomplete search works. Still need to fix tabbing and remove list if search string is updated

--- Assessments.Frontend.Web/wwwroot/js/search.js
+++ Assessments.Frontend.Web/wwwroot/js/search.js
@@ -1,4 +1,5 @@
 const searchField = document.getElementById("Name");
+const autocompleteList = document.getElementById("autocomplete_list_ul");
 const searchUrlBase = "https://artskart.artsdatabanken.no/appapi/api/data/SearchTaxons?";
 const autoCompleteWaitTime = 1500;
 
@@ -37,14 +38,30 @@ const wait = async (ms) => {
     });
 }
 
+const createList = (json) => {
+    autocompleteList.innerHTML = "";
+    json.forEach(el => {
+        const li = document.createElement("li");
+        li.innerHTML = `${el.MatchedName} (${el.TaxonCategory})`;
+        li.classList.add("search_autocomplete");
+        li.tabIndex = 1;
+        li.onclick = () => {
+            searchField.value = el.MatchedName;
+            document.getElementById("search_and_filter_form").submit();
+        }
+        autocompleteList.appendChild(li);
+    });
+    autocompleteList.style["display"] = "block";
+}
+
 let autoCompleteTs;
 const inputChange = async (e) => {
     let json;
     let localTs = Date.now();
     autoCompleteTs = localTs;
-
+    
     const hasStoppedTyping = () => localTs === autoCompleteTs;
-
+    
     if (e.target.value.length < 3) {
         timeStamp = Date.now();
         return;
@@ -63,10 +80,10 @@ const inputChange = async (e) => {
         json = await response.json();
     }
     json = json.map(el => {
-        return {"match": el.MatchedName, "category": taxonCategories[el.TaxonCategory]};
+        return { "MatchedName": el.MatchedName, "TaxonCategory": taxonCategories[el.TaxonCategory]};
     });
 
-    console.log("json", json);
+    createList(json);
 }
 
 searchField.addEventListener("input", inputChange);

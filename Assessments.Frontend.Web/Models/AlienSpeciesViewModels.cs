e46a62e1d12f4d0df829f688c2444f708ea8c61d
Mats Sæter <mats.sater@artsdatabanken.no>
2022-10-13 12:44:35 +0200

    add more parameters to AlienSpeciesListParameters and make a class for Categories (redlist used json file for this)

--- Assessments.Frontend.Web/Models/AlienSpeciesViewModels.cs
+++ Assessments.Frontend.Web/Models/AlienSpeciesViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using Assessments.Mapping.AlienSpecies;
+using System;
 using X.PagedList;
 
 namespace Assessments.Frontend.Web.Models
@@ -12,7 +13,43 @@ namespace Assessments.Frontend.Web.Models
 
     public class AlienSpeciesListParameters
     {
+        public AlienSpeciesListParameters()
+        {
+            this.Area = Array.Empty<string>();
+            this.Category = Array.Empty<string>();
+            this.Criterias = Array.Empty<string>();
+            this.IsCheck = Array.Empty<string>();
+            this.Meta = Array.Empty<string>();
+            this.Regions = Array.Empty<string>();
+            this.Habitats = Array.Empty<string>();
+            this.SpeciesGroups = Array.Empty<string>();
+            this.TaxonRank = Array.Empty<string>();
+        }
+
+        public string[] Area { get; set; }
+
+        public string[] Category { get; set; }
+
+        public string[] Criterias { get; set; }
+
+        public string[] Habitats { get; set; }
+
+        public string[] IsCheck { get; set; }
+
         public string Name { get; set; }
+
+        public string[] Meta { get; set; }
+
+        public string[] Regions { get; set; }
+
+        public string SortBy { get; set; }
+
+        public string[] SpeciesGroups { get; set; }
+
+        public string[] TaxonRank { get; set; }
+
+        public string View { get; set; }
+
     }
 
     public class AlienSpeciesDetailViewModel

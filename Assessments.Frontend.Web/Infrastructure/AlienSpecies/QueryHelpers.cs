95bf804f5e4aa6044a32df46f217b9267ed19c62
Eirik <eirik.niemi@artsdatabanken.no>
2022-10-19 08:50:25 +0200

    Reset alienspeciesassessment model, keep some basic properties (#645)

--- Assessments.Frontend.Web/Infrastructure/AlienSpecies/QueryHelpers.cs
+++ Assessments.Frontend.Web/Infrastructure/AlienSpecies/QueryHelpers.cs
@@ -9,7 +9,7 @@ namespace Assessments.Frontend.Web.Infrastructure.AlienSpecies
         public static IQueryable<AlienSpeciesAssessment2023> ApplyParameters(AlienSpeciesListParameters parameters, IQueryable<AlienSpeciesAssessment2023> query)
         {
             if (!string.IsNullOrEmpty(parameters.Name))
-                query = query.Where(x => x.EvaluatedScientificName.ToLowerInvariant()
+                query = query.Where(x => x.ScientificName.ToLowerInvariant()
                     .Contains(parameters.Name.ToLowerInvariant()));
            
             return query;

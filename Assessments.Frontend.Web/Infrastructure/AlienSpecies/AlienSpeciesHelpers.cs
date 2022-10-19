7f53e7f1c556b5912c402a623fc52c4cfb41bb68
Mats Sæter <mats.sater@artsdatabanken.no>
2022-10-19 10:42:18 +0200

    make files for assessment heading reusable across lists

---
+++ Assessments.Frontend.Web/Infrastructure/AlienSpecies/AlienSpeciesHelpers.cs
@@ -0,0 +1,30 @@
+﻿namespace Assessments.Frontend.Web.Infrastructure.AlienSpecies
+{
+    public class AlienSpeciesHelpers
+    {
+        public static SpeciesGroups.SpeciesGroupItem GetSpeciesGroup(SpeciesGroups.SpeciesGroupItem[] speciesGroups, string speciesGroupName)
+        {
+            for (int i = 0; i < speciesGroups.Length; i++)
+            {
+                SpeciesGroups.SpeciesGroupItem speciesGroup = speciesGroups[i];
+                if (speciesGroup.SpeciesName == speciesGroupName)
+                {
+                    return speciesGroup;
+                }
+
+                if (speciesGroup.SubGroup != null)
+                {
+                    for (int j = 0; j < speciesGroup.SubGroup.Length; j++)
+                    {
+                        SpeciesGroups.SpeciesGroupItem subGroup = speciesGroup.SubGroup[j];
+                        if (subGroup.SpeciesName == speciesGroupName)
+                        {
+                            return subGroup;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}

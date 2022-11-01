2bece4caa145abc5168dc78c8cc63e4711430dfc
Eirik <eirik.niemi@artsdatabanken.no>
2022-11-01 08:50:17 +0100

    Modify evaluation context query, add enum array extension

--- Assessments.Frontend.Web/Infrastructure/AlienSpecies/QueryHelpers.cs
+++ Assessments.Frontend.Web/Infrastructure/AlienSpecies/QueryHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using Assessments.Frontend.Web.Models;
 using Assessments.Mapping.AlienSpecies;
+using Assessments.Shared.Helpers;
 using System;
 using System.Linq;
 
@@ -10,29 +11,20 @@ namespace Assessments.Frontend.Web.Infrastructure.AlienSpecies
         public static IQueryable<AlienSpeciesAssessment2023> ApplyParameters(AlienSpeciesListParameters parameters, IQueryable<AlienSpeciesAssessment2023> query)
         {
             if (!string.IsNullOrEmpty(parameters.Name))
-                query = query.Where(x => x.ScientificName.ToLowerInvariant()
-                    .Contains(parameters.Name.ToLowerInvariant()));
+                query = query.Where(x => x.ScientificName.ToLowerInvariant().Contains(parameters.Name.ToLowerInvariant()));
 
             if (parameters.Area.Any())
-            {
-                query = ApplyAreaFilters(query, parameters.Area);
-            }
+                query = query.Where(x => parameters.Area.ToEnumerable<AlienSpeciesAssessment2023EvaluationContext>().Contains(x.EvaluationContext) && x.AlienSpeciesCategory != "RegionallyAlien"); // remove "regionallyalien" assessments when filtering by evaluation context
 
             if (parameters.Category.Any())
-                query = query.Where(x => parameters.Category.Select(y => (AlienSpeciesAssessment2023Category)Enum.Parse(typeof(AlienSpeciesAssessment2023Category), y)).Contains(x.Category));
+                query = query.Where(x => parameters.Category.ToEnumerable<AlienSpeciesAssessment2023Category>().Contains(x.Category));
 
             if (string.IsNullOrEmpty(parameters.SortBy) || parameters.SortBy.Equals(nameof(AlienSpeciesAssessment2023.ScientificName), StringComparison.InvariantCultureIgnoreCase))
-            {
                 query = query.OrderBy(x => x.ScientificName);
-            }
             else if (parameters.SortBy.Equals(nameof(AlienSpeciesAssessment2023.VernacularName), StringComparison.InvariantCultureIgnoreCase))
-            {
                 query = query.OrderBy(x => x.VernacularName);
-            }
             else if (parameters.SortBy.Equals(nameof(AlienSpeciesAssessment2023.Category), StringComparison.InvariantCultureIgnoreCase))
-            {
                 query = query.OrderBy(x => x.Category);
-            }
 
             return query;
         }
@@ -44,23 +36,6 @@ namespace Assessments.Frontend.Web.Infrastructure.AlienSpecies
                 parameters = new AlienSpeciesListParameters(parameters.IsCheck, parameters.Meta, parameters.Name);
             }
             return parameters;
-        }
-
-        public static IQueryable<AlienSpeciesAssessment2023> ApplyAreaFilters(IQueryable<AlienSpeciesAssessment2023> query, string[] areas)
-        {
-            IQueryable<AlienSpeciesAssessment2023> assessments = Enumerable.Empty<AlienSpeciesAssessment2023>().AsQueryable();
-            string svalbard = Enum.Parse(typeof(AlienSpeciesAssessment2023EvaluationContext), "S").ToString();
-            string norway = Enum.Parse(typeof(AlienSpeciesAssessment2023EvaluationContext), "N").ToString();
-            string regionallyAlien = "RegionallyAlien";
-
-            foreach (var param in areas)
-            {
-                if (param == svalbard)
-                    assessments = assessments.Concat(query.Where(x => x.EvaluationContext.ToString() == param));
-                if (param == norway)
-                    assessments = assessments.Concat(query.Where(x => x.EvaluationContext.ToString() == param && x.AlienSpeciesCategory != regionallyAlien));
-            }
-            return assessments;
         }
     }
 }

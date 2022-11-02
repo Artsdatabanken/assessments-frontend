54f01a269bb95fbee44c0013284e908851658412
Ane <ane.myhre@artsdatabanken.no>
2022-11-02 15:41:07 +0100

    Changed AlienSpeciecCategory from string to enum

---
+++ Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciecAssessment2023AlienSpeciesCategory.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assessments.Mapping.AlienSpecies.Model.Enums
+{
+    public enum AlienSpeciecAssessment2023AlienSpeciesCategory
+    {
+        [Display(Name = "Selvstendig reproduserende")]
+        AlienSpecie,
+
+        [Display(Name = "Dørstokkart")]
+        DoorKnocker,
+
+        [Display(Name = "Økologisk effekt uten selvstendig reproduksjon innen 50 år")] //treated like a doorknocker and will be visualised with "Dørstokkart" in the filter meny, but use this displayname in the species assessment site. 
+        EffectWithoutReproduction,
+
+        [Display(Name = "Regionalt fremmed")] //These are only alien in parts of Norway (they are resident species of Norway) and are therefore excluded from the filter on assessment area (EvaluationContext). 
+        RegionallyAlien,
+
+        [Display(Name = "Ikke fremmed")]
+        NotAlienSpecie,
+
+        [Display(Name = "Vurderes på et annet taksonomisk nivå")]
+        TaxonEvaluatedAtAnotherLevel,
+
+        [Display(Name = "Etablert per år 1800")]
+        UncertainBefore1800,
+
+        // TODO: Remove values below when no assessments have any of these values in AlienSpeciesCategory. At latest, these should be removed before 'innsynet' opens to the public.
+
+        [Display(Name = "Ikke avgjort")]
+        NotDefined,
+
+        [Display(Name = "Ikke avgjort")]
+        NotApplicable,
+
+        [Display(Name = "Ikke avgjort")]
+        EcoEffectWithoutEstablishment
+    }
+}

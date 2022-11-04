0da59386f921b7eb78f52f196a10dca15b8576fa
Eirik <eirik.niemi@artsdatabanken.no>
2022-11-04 08:06:26 +0100

    Add unit test project and automapper test

---
+++ Assessments.Tests/Mapping/AlienSpeciesAssessment2023ProfileTests.cs
@@ -0,0 +1,18 @@
+﻿using Assessments.Mapping.AlienSpecies.Profiles;
+using AutoMapper;
+
+namespace Assessments.Tests.Mapping
+{
+    public class AlienSpeciesAssessment2023ProfileTests
+    {
+        private readonly MapperConfiguration _mapper;
+
+        public AlienSpeciesAssessment2023ProfileTests()
+        {
+            _mapper = new MapperConfiguration(config => config.AddProfile(new AlienSpeciesAssessment2023Profile()));
+        }
+
+        [Fact]
+        public void AlienSpeciesAssessment2023ProfileShouldBeValid() => _mapper.AssertConfigurationIsValid();
+    }
+}

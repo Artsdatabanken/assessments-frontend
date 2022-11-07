a13d9d23e3d26e2657a619c9c778c88738540f31
Ane <ane.myhre@artsdatabanken.no>
2022-11-07 19:53:56 +0100

    Added geographical variation and climate effects to export

---
+++ Assessments.Mapping/AlienSpecies/Helpers/AlienSpeciesAssessment2023ExportProfileHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Assessments.Mapping.AlienSpecies.Helpers
+{
+    internal static class AlienSpeciesAssessment2023ExportProfileHelper
+    {
+        internal static string GetGeographicalVariation(string[] geoVar)
+        {
+            return string.Join(",", geoVar);
+        }
+    }
+}

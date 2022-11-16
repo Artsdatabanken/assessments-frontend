3dcc656e3a271d765eb4ea6d3c74eee9f611d9e2
Eirik <eirik.niemi@artsdatabanken.no>
2022-11-16 13:49:04 +0100

    Add remove unwanted html extension (#720)

---
+++ Assessments.Tests/Extensions/ExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using Assessments.Shared.Helpers;
+using FluentAssertions;
+
+namespace Assessments.Tests.Extensions
+{
+    public class ExtensionTests
+    {
+        [Fact]
+        public void StripHtmlExtensionShouldRemoveHtml()
+        {
+            "<p>Lorem ipsum</p>".StripHtml().Should().Be("Lorem ipsum");
+        }
+        
+        [Fact]
+        public void RemoveUnwantedHtmlTagsExtensionShouldRemoveUnwantedHtmlTags()
+        {
+            "<p>Lorem ipsum</p><div>Lorem ipsum</div>".StripUnwantedHtml().Should().Be("<p>Lorem ipsum</p>Lorem ipsum");
+        }
+    }
+}

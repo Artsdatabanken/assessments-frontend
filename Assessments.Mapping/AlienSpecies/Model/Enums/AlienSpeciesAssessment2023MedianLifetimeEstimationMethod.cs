834c852d985e70a89ad7758f5fa07a8edbd4c348
Ane <ane.myhre@artsdatabanken.no>
2022-12-01 15:56:10 +0100

    estimation method A-criterion as enum

---
+++ Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023MedianLifetimeEstimationMethod.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assessments.Mapping.AlienSpecies.Model.Enums
+{
+    public enum AlienSpeciesAssessment2023MedianLifetimeEstimationMethod
+    {
+        [Display(Name = "Forenklet anslag")]
+        LifespanA1aSimplifiedEstimate,
+
+        [Display(Name = "Numerisk estimering")]
+        SpreadRscriptEstimatedSpeciesLongevity,
+
+        [Display(Name = "Levedyktighetsanalyse")]
+        ViableAnalysis,
+
+        [Display(Name = "")]
+        NotRelevant
+    }
+}

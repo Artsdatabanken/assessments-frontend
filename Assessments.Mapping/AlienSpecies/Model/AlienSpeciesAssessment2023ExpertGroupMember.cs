cd9de96d687910070374cd69f8d4cdac3bfeff7e
Eirik <eirik.niemi@artsdatabanken.no>
2022-12-05 15:43:49 +0100

    Add expertgroups from database to transformation (wip #709)

---
+++ Assessments.Mapping/AlienSpecies/Model/AlienSpeciesAssessment2023ExpertGroupMember.cs
@@ -0,0 +1,9 @@
+﻿namespace Assessments.Mapping.AlienSpecies.Model
+{
+    public class AlienSpeciesAssessment2023ExpertGroupMember
+    {
+        public string ExpertCommittee { get; set; }
+
+        public string Name { get; set; }
+    }
+}

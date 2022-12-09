f24101a8a24478d70ec656fa87b4718a4f5d5f87
Eirik <eirik.niemi@artsdatabanken.no>
2022-12-09 12:35:42 +0100

    Add criterion description extension (#732)

--- Assessments.Mapping/AlienSpecies/Model/AlienSpeciesAssessment2023Criterion.cs
+++ Assessments.Mapping/AlienSpecies/Model/AlienSpeciesAssessment2023Criterion.cs
@@ -3,12 +3,24 @@ using Assessments.Mapping.AlienSpecies.Model.Enums;
 
 namespace Assessments.Mapping.AlienSpecies.Model
 {
+    /// <summary>
+    /// TODO: documentation
+    /// </summary>
     public class AlienSpeciesAssessment2023Criterion
     {
+        /// <summary>
+        /// TODO: documentation
+        /// </summary>
         public AlienSpeciesAssessment2023CriteriaLetter CriteriaLetter { get; set; }
 
+        /// <summary>
+        /// TODO: documentation
+        /// </summary>
         public int Value { get; set; }
 
+        /// <summary>
+        /// TODO: documentation
+        /// </summary>
         public List<int> UncertaintyValues { get; set; }
     }
 }

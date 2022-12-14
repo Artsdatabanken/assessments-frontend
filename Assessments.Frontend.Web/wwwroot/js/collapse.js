062b8716750b7669571cf303babe10a875e00789
Helene Engeness Mørk <helene.mork@artsdatabanken.no>
2022-12-14 09:44:11 +0100

    removed old, updated example

--- Assessments.Frontend.Web/wwwroot/js/collapse.js
+++ Assessments.Frontend.Web/wwwroot/js/collapse.js
@@ -1,104 +1,36 @@
-﻿/*
-     Create an element containing a header element and a div for hiding.
-     The header element will receive span elements injected in front of the heading itself.
-     The div is the element which will be hidden. If you have this structure you can add the id of the parent element to "makeCollapsibleListIds",
-     and it will become collapsible.
-     
-     example structure when adding "example" into the id list:
-     <div id="example">
-        <h3>Header text</h3>
-        <div>collapse me</div>
-     </div>
-*/
+﻿/**
+ * toggleMe toggles an element's grandparent on and off.
+ * A example of expandable box with styling:
+ 
+ <div class="expandable expanded">
+        <div class="expand_header">
+             <h2>TITLE</h2>
+            <button onclick="toggleMe(this)" class="ghost">
+                <span class="material-icons if_expanded">expand_less </span>
+                <span class="material-icons if_collapsed">expand_more </span>
+            </button>
+        </div>
+        <div class="if_expanded expand_content">
+        </div>
+    </div>
+ */
 
-function toggleMe(element){
+
+function toggleMe(element) {
     console.log(element.parentNode);
     parent = element.parentNode.parentNode;
     isExpanded = parent.classList.contains("expanded");
     if (isExpanded) {
-        console.log("yep")
         parent.classList.remove("expanded")
         parent.classList.add("collapsed")
     } else {
-        console.log("nope")
         parent.classList.add("expanded");
         parent.classList.remove("collapsed")
-    }
-        /// TODO FIX        
+    }   
 }
 
 /*
 const makeCollapsibleListIds = [
     'show_expert_summary',
     'show_occurence_area'
-];
-
-const expandLessInnerHtml = 'expand_less';
-const expandMoreInnerHtml = 'expand_more';
-const expandClassList = ['material-icons-outlined'];
-const collapsibleHeaderClass = 'collapsible_header';
-
-const getCollapsibleIcon = isOpen => {
-    const icon = document.createElement('span');
-    icon.classList = expandClassList;
-    icon.innerHTML = isOpen ? expandLessInnerHtml : expandMoreInnerHtml;
-    return icon;
-}
-
-const addCollapsibleArrow = element => {
-    const icon = getCollapsibleIcon(isOpen = false);
-    element.prepend(icon);
-    element.classList.add(collapsibleHeaderClass);
-}
-
-const switchArrowType = element => {
-    const showCollapsedIcon = Array.prototype.some.call(element?.childNodes, el => {
-        return el.innerHTML === expandLessInnerHtml;
-    });
-    element.childNodes[0].remove();
-    element.prepend(getCollapsibleIcon(!showCollapsedIcon));
-}
-
-const toggleCollapsed = element => {
-    isCollapsed = element.style['display'] === 'none';
-    if (isCollapsed)
-        element.style = 'display:block';
-    else
-        element.style = 'display:none';
-}
-
-const collapseElementUsingId = id => {
-    const elements = document.getElementById(id)?.childNodes;
-    const filtered = Array.prototype.filter.call(elements, (el => el.nodeType === Node.ELEMENT_NODE));
-    const [ header, collapsible ] = filtered;
-
-    switchArrowType(header);
-    toggleCollapsed(collapsible);
-}
-
-const addOnclickFunction = (element, id) => {
-    element.onclick = () => {
-        collapseElementUsingId(id);
-    }
-}
-
-
-
-const makeCollapsible = id => {
-    const elements = document.getElementById(id)?.childNodes;
-    if (!elements) return;
-
-    const filtered = Array.prototype.filter.call(elements, (el => el.nodeType === Node.ELEMENT_NODE));
-    if (filtered?.length !== 2) return;
-
-    const [ header, collapsible ] = filtered;
-    addCollapsibleArrow(header);
-    addOnclickFunction(header, id);
-    toggleCollapsed(collapsible);
-}
-
-makeCollapsibleListIds?.forEach(id => {
-    makeCollapsible(id);
-});
-
-*/
+];*/

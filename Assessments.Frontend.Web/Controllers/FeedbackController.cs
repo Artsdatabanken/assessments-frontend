e768b454ec65ae803230823ade10e0db705106d1
Eirik <eirik.niemi@artsdatabanken.no>
2023-01-02 16:01:01 +0100

    Persist files to storage account (wip #791)

--- Assessments.Frontend.Web/Controllers/FeedbackController.cs
+++ Assessments.Frontend.Web/Controllers/FeedbackController.cs
@@ -1,38 +1,58 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Assessments.Frontend.Web.Models;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Assessments.Frontend.Web.Controllers
 {
     public class FeedbackController : BaseController<FeedbackController>
     {
+        private readonly IConfiguration _configuration;
+
+        public FeedbackController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpPost]
-        public IActionResult Index(FeedbackViewModel viewModel, string returnUrl)
+        public async Task<IActionResult> Index(FeedbackViewModel viewModel, string returnUrl)
         {
             if (ModelState.IsValid)
             {
-                TempData["feedback"] = returnUrl;
-                
+                var folderId = Guid.NewGuid();
+
+                if (viewModel.FormFiles.Any())
+                {
+                    var connectionString = _configuration["ConnectionStrings:AzureBlobStorage"];
+                    var blob = new BlobContainerClient(connectionString, "files");
+
+                    await blob.CreateIfNotExistsAsync();
+
+                    string[] permittedExtensions = { ".pdf", ".doc", ".docx" };
+                    
+                    foreach (var formFile in viewModel.FormFiles.Where(x => x.Length <= 0))
+                    {
+                        var extension = Path.GetExtension(formFile.FileName).ToLowerInvariant();
+
+                        if (string.IsNullOrEmpty(extension) || !permittedExtensions.Contains(extension))
+                            continue;
+
+                        await blob.UploadBlobAsync($"{folderId}/{formFile.FileName}", formFile.OpenReadStream());
+                    }
+                }
+
                 // TODO: persist data
 
-                string[] permittedExtensions = { ".pdf", ".doc", ".docx" };
+                TempData["feedback"] = "OK";
 
-                foreach (var formFile in viewModel.FormFiles)
-                {
-                    if (formFile.Length <= 0)
-                        continue;
-
-                    var extension = Path.GetExtension(formFile.FileName).ToLowerInvariant();
-
-                    if (string.IsNullOrEmpty(extension) || !permittedExtensions.Contains(extension))
-                        continue;
-
-                    // TODO: persist file(s)
-                }
+                return Url.IsLocalUrl(returnUrl) ? Redirect(returnUrl) : BadRequest();
             }
 
-            return Url.IsLocalUrl(returnUrl) ? Redirect(returnUrl) : BadRequest();
+            return BadRequest("Beklager, en feil oppstod ved innsending av skjema");
         }
     }
 }

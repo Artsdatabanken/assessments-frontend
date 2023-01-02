2c249c968062b0a3615199be0ee2ef9fed1dd541
Eirik <eirik.niemi@artsdatabanken.no>
2023-01-02 15:00:47 +0100

    Add feedback form, model and controller (wip #791)

---
+++ Assessments.Frontend.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Linq;
+using Assessments.Frontend.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assessments.Frontend.Web.Controllers
+{
+    public class FeedbackController : BaseController<FeedbackController>
+    {
+        [HttpPost]
+        public IActionResult Index(FeedbackViewModel viewModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["feedback"] = returnUrl;
+                
+                // TODO: persist data
+
+                string[] permittedExtensions = { ".pdf", ".doc", ".docx" };
+
+                foreach (var formFile in viewModel.FormFiles)
+                {
+                    if (formFile.Length <= 0)
+                        continue;
+
+                    var extension = Path.GetExtension(formFile.FileName).ToLowerInvariant();
+
+                    if (string.IsNullOrEmpty(extension) || !permittedExtensions.Contains(extension))
+                        continue;
+
+                    // TODO: persist file(s)
+                }
+            }
+
+            return Url.IsLocalUrl(returnUrl) ? Redirect(returnUrl) : BadRequest();
+        }
+    }
+}
---
+++ Assessments.Frontend.Web/Models/FeedbackViewModels.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assessments.Frontend.Web.Models
+{
+    public class FeedbackViewModel
+    {
+        [Required(ErrorMessage = "{0} er påkrevd")]
+        [MinLength(5, ErrorMessage = "{0} må ha minst {1} tegn")]
+        [MaxLength(100, ErrorMessage = "{0} kan ha maks {1} tegn")]
+        [Display(Name = "Navn")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "{0} er påkrevd")]
+        [MaxLength(100, ErrorMessage = "{0} kan ha maks {1} tegn")]
+        [EmailAddress(ErrorMessage = "{0} må være en gyldig e-postadresse")]
+        [Display(Name = "E-postadresse")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} er påkrevd")]
+        [MinLength(5, ErrorMessage = "{0} må ha minst {1} tegn")]
+        [MaxLength(1024, ErrorMessage = "{0} kan ha maks {1} tegn")]
+        [Display(Name = "Kommentar")]
+        public string Comment { get; set; }
+        
+        [Required, HiddenInput]
+        public int AssessmentId { get; set; }
+
+        [Required, HiddenInput]
+        public int Year { get; set; }
+
+        [Required, HiddenInput]
+        public string ExpertGroup { get; set; }
+
+        [Required, HiddenInput]
+        public FeedbackType Type { get; set; }
+
+        [Display(Name = "Filer")]
+        public List<IFormFile> FormFiles { get; set; }
+    }
+
+    public enum FeedbackType
+    {
+        AlienSpecies,
+        Species
+    }
+}

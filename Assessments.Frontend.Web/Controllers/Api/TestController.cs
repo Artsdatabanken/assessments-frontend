7c86ba820a6b139e6e2825661e3f478bc5a4330b
Eirik <eirik.niemi@artsdatabanken.no>
2023-01-12 14:23:57 +0100

    Add temporary databaseinfo (troubleshooting connectionstring)

--- Assessments.Frontend.Web/Controllers/Api/TestController.cs
+++ Assessments.Frontend.Web/Controllers/Api/TestController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace Assessments.Frontend.Web.Controllers.Api;
 
@@ -8,12 +10,24 @@ namespace Assessments.Frontend.Web.Controllers.Api;
 public class TestController : Controller
 {
     private readonly IWebHostEnvironment _environment;
+    private readonly IConfiguration _configuration;
 
-    public TestController(IWebHostEnvironment environment)
+    public TestController(IWebHostEnvironment environment, IConfiguration configuration
+    )
     {
+        _configuration = configuration;
         _environment = environment;
     }
 
-    [HttpGet]
-    public IActionResult Get() => Ok($"Hello from {_environment.EnvironmentName.ToLower()} environment!");
+    [HttpGet("")]
+    public IActionResult Test() => Ok($"Hello from {_environment.EnvironmentName.ToLower()} environment!");
+
+    [HttpGet("database")]
+    public IActionResult DatabaseInfo()
+    {
+        var connectionString = _configuration.GetConnectionString("Default");
+        var sqlConnectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+        
+        return Ok($"DataSource: {sqlConnectionStringBuilder.DataSource}, UserID: {sqlConnectionStringBuilder.UserID}, InitialCatalog:  {sqlConnectionStringBuilder.InitialCatalog}");
+    }
 }

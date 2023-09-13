0d7e243e5bc97fc70676d96a6a570f5efd4e4d31
bolsson <bo.olesen@artsdatabanken.no>
2023-09-13 14:34:28 +0200

    #1531 La til model DynamicProperty

---
+++ Assessments.Transformation/Models/DynamicProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessments.Transformation.Models
+{
+    internal class DynamicProperty
+    {
+        public class Property
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public Property[] Properties { get; set; }
+        }
+
+        public string Id { get; set; }
+        public string[] References { get; set; }
+
+        public Property[] Properties { get; set; }
+    }
+}
+

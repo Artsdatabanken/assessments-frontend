107d5eff2931de4075eefa51940debb4faa07e80
bolsson <bo.olesen@artsdatabanken.no>
2023-11-07 08:42:21 +0100

    #1351 Refactoring to more efficient delete and update to RavenDb

--- Assessments.Transformation/PublishDynamicProperties.cs
+++ Assessments.Transformation/PublishDynamicProperties.cs
@@ -18,6 +18,7 @@ using System;
 using Raven.Abstractions.Commands;
 using static Assessments.Mapping.RedlistSpecies.Source.Rodliste2019;
 using Raven.Abstractions.Extensions;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Assessments.Transformation
 {
@@ -66,9 +67,9 @@ namespace Assessments.Transformation
         public async Task<List<DynamicProperty>> ImportAlienList2023()
         {
             var fileContent = await Storage.DownloadFromBlob(_configuration, DataFilenames.AlienSpecies2023);
-            var speciesAssessments = JsonSerializer.Deserialize<IList<AlienSpeciesAssessment2023>>(fileContent)?.AsQueryable();
+            var AlienSpeciesAssessments = JsonSerializer.Deserialize<IList<AlienSpeciesAssessment2023>>(fileContent)?.AsQueryable();
 
-            return  speciesAssessments.Select(x => new DynamicProperty
+            return  AlienSpeciesAssessments.Select(x => new DynamicProperty
             {
                 Id = "DynamicProperty/FremmedArt2023-" + x.Id.ToString(),
                 References = new[] { "ScientificNames/" + x.ScientificName.ScientificNameId.ToString() },
@@ -86,7 +87,7 @@ namespace Assessments.Transformation
                             new DynamicProperty.Property() { Name = "Område", Value = x.EvaluationContext.DisplayName()  },
                             new DynamicProperty.Property() { Name = "Aar", Value = "2023" },
                             new DynamicProperty.Property() { Name = "Url", Value = "https://artsdatabanken.no/lister/fremmedartslista/2023/" + x.Id.ToString() },
-                            new DynamicProperty.Property() { Name = "Fremmedartsstatus" , Value = x.AlienSpeciesCategory.ToString()} //TODO, IMPORTANT! - check that this tranformation is correct
+                            new DynamicProperty.Property() { Name = "Fremmedartsstatus" , Value = x.AlienSpeciesCategory.ToString()}
                         }
                     }
                 }
@@ -106,28 +107,38 @@ namespace Assessments.Transformation
             //ravenSession.Load<DynamicProperty>("\"DynamicProperty/FremmedArt2023-\"");//NOTE: we might need a Databank.Domain.Content.Node type here from data.artsdatabanken.no
 
 
-            DeleteExistingDynamicProperties(ravenSession, "DynamicProperty/Rodliste2021");
-            DeleteExistingDynamicProperties(ravenSession, "DynamicProperty/Fremmedart2023"); //ravenSession.Query<DynamicProperty>().Where(x => x.Id.StartsWith("DynamicProperty/Fremmedart2023")).Skip(pointer).Take(batchSize).ToArray();
+            var existingDynamicProperties = GetExistingDynamicProperties(ravenSession, "DynamicProperty/Rodliste2021");
+            existingDynamicProperties.AddRange(GetExistingDynamicProperties(ravenSession, "DynamicProperty/Fremmedart2023")); //ravenSession.Query<DynamicProperty>().Where(x => x.Id.StartsWith("DynamicProperty/Fremmedart2023")).Skip(pointer).Take(batchSize).ToArray();
 
-            //// Delete dynamicProperties in RavenDb that do not exist in the latest dynamicProperty collection
+            var comparer = new DynamicPropertyComparer();
+            var obsoletDynamicProperties = existingDynamicProperties.Except(dynamicPropertiesFromStorageAccount, comparer).ToList();
 
-            //// GET dynamicProperties from RavenDb pertaining alien species 2023 assessments
-            //var queryAlienSpecies2023 = ravenSession.Advanced.DocumentQuery<DynamicProperty>("Raven/DocumentsByEntityName")
-            //    .WhereStartsWith("__document_id", "DynamicProperty/FremmedArt2023-").ToList();
-
-            //var queryRedlistedSpecies2021 = ravenSession.Advanced.DocumentQuery<DynamicProperty>("Raven/DocumentsByEntityName")
-            //    .WhereStartsWith("__document_id", "DynamicProperty/Rodliste2021-").ToList();
-
-            //var existingDynamicProperties = queryAlienSpecies2023.Union(queryRedlistedSpecies2021).ToList();
-
-            ////TODO: Delete this list of dynamicProperties from RavenDb
-            //var obsoleteDynamicProperties = newDynamicProperties.Except(existingDynamicProperties).ToList();
-
+            DeleteDynamicProperties(ravenSession, obsoletDynamicProperties);
 
             //Store DynamicProperties in RavenDb
-            StoreDynamicProperties(dynamicPropertiesFromStorageAccount, ravenSession);
+            StoreDynamicProperties(dynamicPropertiesFromStorageAccount, ravenSession); //NOTE, check if existing dynamicProperties in Raven are overwritten
 
             ravenSession.SaveChanges();
+        }
+
+        public class DynamicPropertyComparer : IEqualityComparer<DynamicProperty>
+        {
+            public bool Equals(DynamicProperty x, DynamicProperty y)
+            {
+                return x.Id == y.Id;
+            }
+
+            public int GetHashCode([DisallowNull] DynamicProperty obj)
+            {
+                return obj.Id.GetHashCode();
+            }
+        }
+
+            private static void DeleteDynamicProperties(IDocumentSession ravenSession, List<DynamicProperty> obsoletDynamicProperties)
+        {
+            // Delete dynamicProperties in RavenDb that do not exist in the latest dynamicProperty collection
+            foreach (var item in obsoletDynamicProperties)
+                ravenSession.Delete<DynamicProperty>(item);
         }
 
         private static void StoreDynamicProperties(List<DynamicProperty> newDynamicProperties, IDocumentSession ravenSession)
@@ -138,24 +149,27 @@ namespace Assessments.Transformation
             }
         }
 
-        private static void DeleteExistingDynamicProperties(IDocumentSession ravenSession, string firstPartOfDocumentName)
+        private static List<DynamicProperty> GetExistingDynamicProperties(IDocumentSession ravenSession, string firstPartOfDocumentName)
         {
             int pointer = 0;
             const int batchSize = 1024;
 
-            List<DynamicProperty> getRedlistet2021;
+            List<DynamicProperty> assessmentsBatch;
+            List<DynamicProperty> allAssessments = new List<DynamicProperty>();
+
             while (true)
             {
                 //Page through the dynamicPropertis as RavenDb server per default has a limit of 128 = 2^7 result (batchSize)
-                getRedlistet2021 = ravenSession.Query<DynamicProperty>().Where(x => x.Id.StartsWith(firstPartOfDocumentName)).Skip(pointer).Take(batchSize).ToList();
+                assessmentsBatch = ravenSession.Query<DynamicProperty>().Where(x => x.Id.StartsWith(firstPartOfDocumentName)).Skip(pointer).Take(batchSize).ToList();
+                
+                if (!assessmentsBatch.Any()) break;
 
-                if (!getRedlistet2021.Any()) break;
-
-                foreach (var item in getRedlistet2021)
-                    ravenSession.Delete<DynamicProperty>(item);
+                allAssessments.AddRange(assessmentsBatch);
 
                 pointer += batchSize;
             }
+
+            return allAssessments;
         }
     }
 }

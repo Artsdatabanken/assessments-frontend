923c5c4c6197c0da7707ad7215b035caf80c55ec
bo <bo.olesen@artsdatabanken.no>
2023-11-28 09:01:13 +0100

    #1351 Created IEqualityComparers and dedicated comparers to be used on the DynamicProperty object

--- Assessments.Transformation/PublishDynamicProperties.cs
+++ Assessments.Transformation/PublishDynamicProperties.cs
@@ -101,7 +101,6 @@ namespace Assessments.Transformation
             var dynamicPropertiesFromStorageAccount = await publishDynamicProperties.ImportAlienList2023();
             dynamicPropertiesFromStorageAccount.AddRange(await publishDynamicProperties.ImportRedlist2021());
 
-            var DocumentStore = DocumentStoreHolder.Store;
             var ravenSession = DocumentStoreHolder.RavenSession;
 
             //ravenSession.Load<DynamicProperty>("\"DynamicProperty/FremmedArt2023-\"");//NOTE: we might need a Databank.Domain.Content.Node type here from data.artsdatabanken.no
@@ -110,18 +109,18 @@ namespace Assessments.Transformation
             var existingDynamicProperties = GetExistingDynamicProperties(ravenSession, "DynamicProperty/Rodliste2021");
             existingDynamicProperties.AddRange(GetExistingDynamicProperties(ravenSession, "DynamicProperty/Fremmedart2023")); //ravenSession.Query<DynamicProperty>().Where(x => x.Id.StartsWith("DynamicProperty/Fremmedart2023")).Skip(pointer).Take(batchSize).ToArray();
 
-            var comparer = new DynamicPropertyComparer();
+            var comparer = new DynamicPropertyIDComparer();
             var obsoletDynamicProperties = existingDynamicProperties.Except(dynamicPropertiesFromStorageAccount, comparer).ToList();
 
             DeleteDynamicProperties(ravenSession, obsoletDynamicProperties);
 
             //Store DynamicProperties in RavenDb
-            StoreDynamicProperties(dynamicPropertiesFromStorageAccount, ravenSession); //NOTE, check if existing dynamicProperties in Raven are overwritten
+            StoreDynamicProperties(dynamicPropertiesFromStorageAccount, ravenSession); //NOTE, check if existing dynamicProperties in Raven are overwritten? Nope, an error is generated as the Id already exist and can not be stored again!
 
             ravenSession.SaveChanges();
         }
 
-        public class DynamicPropertyComparer : IEqualityComparer<DynamicProperty>
+        public class DynamicPropertyIDComparer : IEqualityComparer<DynamicProperty>
         {
             public bool Equals(DynamicProperty x, DynamicProperty y)
             {
@@ -130,11 +129,83 @@ namespace Assessments.Transformation
 
             public int GetHashCode([DisallowNull] DynamicProperty obj)
             {
+                if (obj == null) { return 0; }
+
                 return obj.Id.GetHashCode();
             }
         }
 
-            private static void DeleteDynamicProperties(IDocumentSession ravenSession, List<DynamicProperty> obsoletDynamicProperties)
+        public class DynamicPropertyObjectComparer : IEqualityComparer<DynamicProperty>
+        {
+            public bool Equals(DynamicProperty x, DynamicProperty y)
+            {
+                // If both are null, or both are the same instance, return true
+                if (ReferenceEquals(x, y))
+                {
+                    return true;
+                }
+
+                // If one is null, return false
+                if (x is null || y is null)
+                {
+                    return false;
+                }
+
+                return x.Id == y.Id && CompareArrays(x.References, y.References) && CompareProperties(x.Properties, y.Properties);
+            }
+
+            public int GetHashCode([DisallowNull] DynamicProperty obj)
+            {
+                if (obj == null) { return 0; }
+
+                return obj.Id.GetHashCode();
+            }
+        }
+
+        private static bool CompareArrays(string[] x, string[] y)
+        {
+            // Check if both arrays are null
+            if (x == null && y == null)
+                return true;
+            // Check if either array is null
+            if (x == null || y == null)
+                return false;
+            // Check if both arrays have the same length
+            if (x.Length != y.Length)
+                return false;
+            // Loop through the elements of both arrays
+            for (int i = 0; i < x.Length; i++)
+            {
+                // Compare the elements using the default string comparer
+                if (!string.Equals(x[i], y[i]))
+                    return false;
+            }
+            // If no difference is found, return true
+            return true;
+        }
+
+        private static bool CompareProperties(DynamicProperty.Property[] x, DynamicProperty.Property[] y)
+        {
+            bool areEqual = true;
+
+            if (x == y) return true;
+            if (x == null || y == null || x.Length != y.Length) return false;
+
+            
+            // Compare the elements using the Equals method
+            for (int i = 0; i > x.Length; i++)
+            {
+                if ( x[i].Name != y[i].Name || x[i].Value != y[i].Value || !CompareProperties(x[i].Properties, y[i].Properties) )
+                {
+                    areEqual = false;
+                    break;
+                }
+            }
+
+            return areEqual;
+        }
+
+        private static void DeleteDynamicProperties(IDocumentSession ravenSession, List<DynamicProperty> obsoletDynamicProperties)
         {
             // Delete dynamicProperties in RavenDb that do not exist in the latest dynamicProperty collection
             foreach (var item in obsoletDynamicProperties)

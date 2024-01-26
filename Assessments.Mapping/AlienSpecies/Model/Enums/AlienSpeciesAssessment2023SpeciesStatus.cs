414054ca4f4bb20b50d54974401c03940f36d070
Eirik <eirik.niemi@artsdatabanken.no>
2024-01-26 11:08:30 +0100

    Remove localized description, add translation to description extension (#1381)

--- Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023SpeciesStatus.cs
+++ Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023SpeciesStatus.cs
@@ -1,43 +1,34 @@
 ﻿using Assessments.Shared.Resources.Enums.AlienSpecies;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using Assessments.Shared.Helpers;
 
 namespace Assessments.Mapping.AlienSpecies.Model.Enums
 {
     public enum AlienSpeciesAssessment2023SpeciesStatus
     {
-        [Display(Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.not_indicated), ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.not_indicated))]
         [Description("")]
         NotIndicated,
 
-        [Display(Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.not_in_Norway), ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource))] 
-        [LocalizedDescription(nameof(AlienSpeciesAssessment2023SpeciesStatusResource.not_in_Norway_description), typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.not_in_Norway), Description = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.not_in_Norway_description))] 
         Abroad,
 
-        [Display(Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.indoors), ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
-        [LocalizedDescription(nameof(AlienSpeciesAssessment2023SpeciesStatusResource.indoors_description), typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.indoors), Description = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.indoors_description))]
         B1,
 
-        [Display(Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.outdoors_production_area), ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
-        [LocalizedDescription(nameof(AlienSpeciesAssessment2023SpeciesStatusResource.outdoors_production_area_description), typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.outdoors_production_area), Description = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.outdoors_production_area_description))]
         B2,
 
-        [Display(Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.documented_Norwegian_nature),
-            ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
-        [LocalizedDescription(nameof(AlienSpeciesAssessment2023SpeciesStatusResource.documented_Norwegian_nature_description), typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.documented_Norwegian_nature), Description = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.documented_Norwegian_nature_description))]
         C0,
         
-        [Display(Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.survive_winter_outdoors), ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
-        [LocalizedDescription(nameof(AlienSpeciesAssessment2023SpeciesStatusResource.survive_winter_outdoors_description), typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.survive_winter_outdoors), Description = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.survive_winter_outdoors_description))]
         C1,
 
-        [Display(Name = nameof(AlienSpeciesAssessment2023AlienSpeciesCategoryResource.alien_species), ResourceType = typeof(AlienSpeciesAssessment2023AlienSpeciesCategoryResource))]
-        [LocalizedDescription(nameof(AlienSpeciesAssessment2023SpeciesStatusResource.reproduces_unaidedly_description), typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023AlienSpeciesCategoryResource.alien_species), Description = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.reproduces_unaidedly_description))]
         C2,
 
-        [Display(Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.established), ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
-        [LocalizedDescription(nameof(AlienSpeciesAssessment2023SpeciesStatusResource.established_description), typeof(AlienSpeciesAssessment2023SpeciesStatusResource))]
+        [Display(ResourceType = typeof(AlienSpeciesAssessment2023SpeciesStatusResource), Name = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.established), Description = nameof(AlienSpeciesAssessment2023SpeciesStatusResource.established_description))]
         C3
     }
 }

ef0eb9e6a41a9f89252988006fa099481ebfbe0b
Ane Marlene Myhre <ane.myhre@artsdatabanken.no>
2024-03-15 07:32:37 +0100

    translate enum ArrivedCountryFrom

--- Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023ArrivedCountryFrom.cs
+++ Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023ArrivedCountryFrom.cs
@@ -1,19 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Assessments.Shared.Resources.Enums.AlienSpecies;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assessments.Mapping.AlienSpecies.Model.Enums
 {
     public enum AlienSpeciesAssessment2023ArrivedCountryFrom
     {
-        [Display(Name = "Fastlands-Norge")]
+        [Display(Name = nameof(AlienSpeciesAssessment2023ArrivedCountryFromResource.other_region), ResourceType = typeof(AlienSpeciesAssessment2023ArrivedCountryFromResource))]
         OtherRegion,
 
-        [Display(Name = "Opprinnelsessted (utlandet)")]
+        [Display(Name = nameof(AlienSpeciesAssessment2023ArrivedCountryFromResource.origin), ResourceType = typeof(AlienSpeciesAssessment2023ArrivedCountryFromResource))]
         Origin,
 
-        [Display(Name = "Annet sted (utlandet)")]
+        [Display(Name = nameof(AlienSpeciesAssessment2023ArrivedCountryFromResource.other), ResourceType = typeof(AlienSpeciesAssessment2023ArrivedCountryFromResource))]
         Other,
 
-        [Display(Name = "Ukjent")]
+        [Display(Name = nameof(AlienSpeciesAssessment2023ArrivedCountryFromResource.unknown), ResourceType = typeof(AlienSpeciesAssessment2023ArrivedCountryFromResource))]
         Unknown,
     }
 }

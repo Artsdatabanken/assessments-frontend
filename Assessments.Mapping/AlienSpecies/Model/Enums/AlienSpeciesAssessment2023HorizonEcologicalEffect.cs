2e1a5532bd5572f4697a48de57b472e1731f39fc
Mats Sæter <mats.sater@artsdatabanken.no>
2024-08-26 15:05:18 +0200

    add display names to enums for horizon scanned values

--- Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023HorizonEcologicalEffect.cs
+++ Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023HorizonEcologicalEffect.cs
@@ -1,8 +1,15 @@
-﻿namespace Assessments.Mapping.AlienSpecies.Model.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assessments.Mapping.AlienSpecies.Model.Enums;
 
 public enum AlienSpeciesAssessment2023HorizonEcologicalEffect
 {
+    [Display(Name = "Fraværende")]
     No,
+
+    [Display(Name = "Kjent eller antatt, men bare så lenge arten er til stede. Det vil si at effekten utgår når arten forsvinner.")]
     YesWhilePresent,
+
+    [Display(Name = "Kjent eller antatt, og effekten vedvarer også etter at arten er borte.")]
     YesAfterGone
 }
--- Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023HorizonEstablismentPotential.cs
+++ Assessments.Mapping/AlienSpecies/Model/Enums/AlienSpeciesAssessment2023HorizonEstablismentPotential.cs
@@ -1,8 +1,15 @@
-﻿namespace Assessments.Mapping.AlienSpecies.Model.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assessments.Mapping.AlienSpecies.Model.Enums;
 
 public enum AlienSpeciesAssessment2023HorizonEstablismentPotential
 {
-    Zero  = 0,
-    One = 1,
-    TwoOrMore = 2
+    [Display(Name = "Ingen. Det vil si at arten har dødd ut fra introduksjonsstedet.")]
+    Zero,
+
+    [Display(Name = "En. Det vil si at arten har verken dødd ut eller ekspandert.")]
+    One,
+
+    [Display(Name = "To eller flere. Det vil si at arten har ekspandert.")]
+    TwoOrMore
 }

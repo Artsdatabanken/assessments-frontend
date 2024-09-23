b370b4c5269d98d4537dc55c7761a78b8c1c184f
Eirik <eirik.niemi@artsdatabanken.no>
2024-09-23 10:17:48 +0200

    Remove temporary test names

--- Assessments.Shared/Helpers/DataFilenames.cs
+++ Assessments.Shared/Helpers/DataFilenames.cs
@@ -13,13 +13,11 @@
 
         public const string SpeciesExpertCommitteeMembers = "species-experts.csv";
 
-        // TODO: midlertidige navn for horisontskanning, endre tilbake etter testing
+        public const string AlienSpecies2023 = "alienspecies-2023.json";
 
-        public const string AlienSpecies2023 = "alienspecies-2023-test.json";
+        public const string AlienSpecies2023Temp = "alienspecies-temp-2023.json";
 
-        public const string AlienSpecies2023Temp = "alienspecies-temp-2023-test.json";
-
-        public const string AlienSpeciesExpertCommitteeMembers = "alienspecies-experts-test.csv";
+        public const string AlienSpeciesExpertCommitteeMembers = "alienspecies-experts.csv";
 
         public static string CalculateAlienSpecies2023AttachmentFilePath(int attachmentId, string fileName)
         {

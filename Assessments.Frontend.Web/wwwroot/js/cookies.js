a822299acfe4e3f65c1e377cebaed1a274d320a8
Stein Hoem <stein.hoem@artsdatabanken.no>
2025-02-21 13:34:44 +0100

    Fix a bit cookies sak

--- Assessments.Frontend.Web/wwwroot/js/cookies.js
+++ Assessments.Frontend.Web/wwwroot/js/cookies.js
@@ -47,3 +47,54 @@ window.addEventListener('CookieInformationConsentGiven', function (event) {
         })();
     }
 }, false);
+
+/* 
+ * Cookies for Google Analytics and their handling
+ * Using session-storage to remember which user preference. 
+ * These fall under criteria B for not needing warning as they are 1. session and B. remembers choice across page.
+ * TODO: Follow best practices: https://www.cookieyes.com/google-analytics-cookie-banner/
+ */
+
+var acceptedcookies = null;
+var readValue = sessionStorage['acceptedcookies'];
+var acceptedcookie = getCookie("acceptedcookie");
+
+// Duration for all persistent cookies should be the same.
+// Set to 365 days (12 months), based on GDPR cookie consent rules.
+var cookieDurationDays = 365;
+var cookieDurationSeconds = cookieDurationDays * 24 * 60 * 60; // GA uses seconds
+var cookieDuration = new Date();
+cookieDuration.setTime(cookieDuration.getTime() + (cookieDurationSeconds * 1000));
+var cookieDurationString = cookieDuration.toUTCString(); // setCookie uses this one.
+
+
+// Make a new cookie
+function setCookie(cname, cvalue, cduration) {
+    let expires = "expires=" + cduration;
+    const secure = "Secure"
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";" + secure + ";path=/";
+}
+
+// Read a cookie
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function themeCookie() {
+    if (document.body) {
+        let currenttheme = document.body.classList || " ";
+        setCookie("theme", currenttheme, cookieDurationString);
+    }
+}

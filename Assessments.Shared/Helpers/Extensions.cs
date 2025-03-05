d54603071e4f1a173193351425ecd4593a09549a
Eirik <eirik.niemi@artsdatabanken.no>
2025-03-05 10:26:31 +0100

    Adjustments

--- Assessments.Shared/Helpers/Extensions.cs
+++ Assessments.Shared/Helpers/Extensions.cs
@@ -5,61 +5,60 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 
-namespace Assessments.Shared.Helpers
+namespace Assessments.Shared.Helpers;
+
+public static class Extensions
 {
-    public static class Extensions
+    public static string DisplayName(this MemberInfo property)
     {
-        public static string DisplayName(this MemberInfo property)
+        var attribute = property.GetCustomAttributes(typeof(DisplayNameAttribute), true).Cast<DisplayNameAttribute>().Single();
+
+        return attribute.DisplayName;
+    }
+
+    private static T GetAttribute<T>(this Enum value) where T : Attribute
+    {
+        var type = value.GetType();
+        var memberInfo = type.GetMember(value.ToString());
+        var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+
+        if (attributes.Length > 0)
+            return (T) attributes[0];
+
+        return null;
+    }
+
+    public static string Description(this Enum value)
+    {
+        var attribute = value.GetAttribute<DisplayAttribute>();
+
+        if (attribute != null)
         {
-            var attribute = property.GetCustomAttributes(typeof(DisplayNameAttribute), true).Cast<DisplayNameAttribute>().Single();
-
-            return attribute.DisplayName;
-        }
-
-        private static T GetAttribute<T>(this Enum value) where T : Attribute
-        {
-            var type = value.GetType();
-            var memberInfo = type.GetMember(value.ToString());
-            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
-
-            if (attributes.Length > 0)
-                return (T) attributes[0];
-
-            return null;
-        }
-
-        public static string Description(this Enum value)
-        {
-            var attribute = value.GetAttribute<DisplayAttribute>();
-
-            if (attribute != null)
-            {
-                return attribute switch
-                {
-                    { ResourceType: not null, Description: not null } => ResourceManagerHelper.GetManager(attribute.ResourceType).GetString(attribute.Description),
-                    _ => attribute.Description ?? value.ToString()
-                };
-            }
-
-            var descriptionAttribute = value.GetAttribute<DescriptionAttribute>();
-            return descriptionAttribute == null ? value.ToString() : descriptionAttribute.Description;
-
-        }
-
-        public static string DisplayName(this Enum value)
-        {
-            var attribute = value.GetAttribute<DisplayAttribute>();
-
             return attribute switch
             {
-                { ResourceType: not null, Name: not null } => ResourceManagerHelper.GetManager(attribute.ResourceType).GetString(attribute.Name),
-                _ => attribute?.Name ?? value.ToString()
+                { ResourceType: not null, Description: not null } => ResourceManagerHelper.GetManager(attribute.ResourceType).GetString(attribute.Description),
+                _ => attribute.Description ?? value.ToString()
             };
         }
 
-        public static IEnumerable<T> ToEnumerable<T>(this IEnumerable<string> array)
+        var descriptionAttribute = value.GetAttribute<DescriptionAttribute>();
+            
+        return descriptionAttribute == null ? value.ToString() : descriptionAttribute.Description;
+    }
+
+    public static string DisplayName(this Enum value)
+    {
+        var attribute = value.GetAttribute<DisplayAttribute>();
+
+        return attribute switch
         {
-            return array.Where(c => Enum.IsDefined(typeof(T), c)).Select(a => (T) Enum.Parse(typeof(T), a));
-        }
+            { ResourceType: not null, Name: not null } => ResourceManagerHelper.GetManager(attribute.ResourceType).GetString(attribute.Name),
+            _ => attribute?.Name ?? value.ToString()
+        };
+    }
+
+    public static IEnumerable<T> ToEnumerable<T>(this IEnumerable<string> array)
+    {
+        return array.Where(c => Enum.IsDefined(typeof(T), c)).Select(a => (T) Enum.Parse(typeof(T), a));
     }
 }
--- Assessments.Shared/Helpers/ResourceManagerHelper.cs
+++ Assessments.Shared/Helpers/ResourceManagerHelper.cs
@@ -12,14 +12,12 @@ public class ResourceManagerHelper
 
     private static ResourceManagerHelper Instance { get { return Lazy.Value; } }
 
-    public static ResourceManager GetManager(Type resourceType)
-    {
-        return Instance.GetResourceManager(resourceType);
-    }
+    public static ResourceManager GetManager(Type resourceType) => Instance.GetResourceManager(resourceType);
 
     private ResourceManager GetResourceManager(Type resourceType)
     {
-        static ResourceManager ValueFactory(Type resourceType) => new(resourceType);
         return _managers.GetOrAdd(resourceType, ValueFactory);
+        
+        static ResourceManager ValueFactory(Type resourceType) => new(resourceType);
     }
 }
